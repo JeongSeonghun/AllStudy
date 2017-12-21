165283dd58b52678b3f89390dacfbe9a35c6832b
jshun <shun6889@emgram.com>
2017-12-21 19:20:05 +0900

    1) engstudy 1.1) edit db quary creater

--- engstudy/src/main/java/com/jshstudy/allstudy/data/QueryCreater.java
+++ engstudy/src/main/java/com/jshstudy/allstudy/data/QueryCreater.java
@@ -18,11 +18,13 @@ public class QueryCreater {
     private final String NAME_TABLE = "TABLE ";
 
     private final String PARAM_INTO = "INTO ";
-    private final String PARAM_SET = "SET ";
-    private final String PARAM_WHERE = "WHERE ";
-    private final String PARAM_ORDER = "ORDER ";
-    private final String PARAM_LIMIT = "LIMIT ";
-    private final String PARAM_OFFSET = "OFFSET ";
+    private final String PARAM_VALUES = " VALUES";
+    private final String PARAM_SET = " SET ";
+    private final String PARAM_WHERE = " WHERE ";
+    private final String PARAM_FROM = " FROM ";
+    private final String PARAM_ORDER = " ORDER ";
+    private final String PARAM_LIMIT = " LIMIT ";
+    private final String PARAM_OFFSET = " OFFSET ";
 
     public String createTable(String tableName, Row row){
         ArrayList<Row> rows = new ArrayList<>();
@@ -32,37 +34,89 @@ public class QueryCreater {
 
     // CREATE TABLE table (row1 typ1 option..., row2...)
     public String createTable(String tableName, ArrayList<Row> rows){
-        if(!checkEmty(tableName) || rows == null || rows.size()<1) return null;
+        if(!checkParams(tableName) || checkParams(rows)) return null;
 
-        return FUNC_CREATE + NAME_TABLE + tableName + wrap1(rows);
+        return FUNC_CREATE + NAME_TABLE + tableName + wrapCreateTable(rows);
+    }
+
+    public String createTableNotExist(String tableName, ArrayList<Row> rows){
+        if(!checkParams(tableName) || checkParams(rows)) return null;
+
+        return FUNC_CREATE + NAME_TABLE + Option.NOT_EXIST.getValue() + tableName + wrapCreateTable(rows);
     }
 
     // INSERT INTO table VALUES(val1, val2...)
     // INSERT INTO table (row1, row2...) VALUES(val1, val2...)
     public String insert(String tableName, ArrayList<Row> rows, ArrayList<String> values){
-        if(!checkEmty(tableName)||rows==null || values == null || rows.size() == values.size()) return null;
+        if(!checkParams(tableName)||checkParams(rows, values) || rows.size() == values.size()) return null;
 
-        return FUNC_INSERT + PARAM_INTO + tableName + wrap2(rows) + wrap3(values);
+        return FUNC_INSERT + PARAM_INTO + tableName + wrapUnit(rows) +PARAM_VALUES+ wrapBase(values);
+    }
+
+    public String insert(String tableName, ArrayList<String> values){
+        if(!checkParams(tableName) || !checkParams(values)) return null;
+
+        return FUNC_INSERT + PARAM_INTO + tableName + PARAM_VALUES + wrapBase(values);
     }
 
     // UPDATE table SET row1 = val1, row2 = val2...
     // WHERE ...
-    public String update(){
-        return null;
+    public String update(String tableName, ArrayList<UpdateData> datas, String where){
+        if(!checkParams(tableName, where) || !checkParams(datas)) return null;
+
+        return FUNC_UPDATE + tableName + PARAM_SET+ wrapUpdate(datas) +(!checkEmty(where)?(PARAM_WHERE+where):null);
     }
 
-    // SELECT
-    public String select(){
-        return null;
+    // SELECT * FROM table
+    // SELECT row1, row2... FROM table
+    // WHERE ...
+    public String select(String tableName, ArrayList<Row> rows, int... limits){
+        if(!checkParams(tableName) || !checkParams(rows)) return null;
+
+        return FUNC_SELECT + wrapUnit(rows) + PARAM_FROM + tableName +addLimit(limits);
     }
 
-    // DELETE
-    public String delete(){
-        return null;
+    public String select(String tableName, int... limits){
+        if(!checkParams(tableName)) return null;
+
+        return FUNC_SELECT + "*" + PARAM_FROM + tableName + addLimit(limits);
     }
 
-    // DROP
+    private String addLimit(int... limits){
+        if(limits==null || limits.length<=0) return null;
+
+        int size = limits.length;
+        if(size == 1){
+            return PARAM_LIMIT + limits[0];
+        }else if(size == 2){
+            return PARAM_LIMIT + limits[0] + PARAM_OFFSET + limits[1];
+        }else{
+            return null;
+        }
+    }
+
+    // DELETE FROM table
+    // DELETE FROM table WHERE ...
+    public String delete(String tableName, String where){
+        if(!checkParams(tableName, where)) return null;
+
+        return FUNC_DELETE + PARAM_FROM + tableName + PARAM_WHERE + where;
+    }
+
+    public String delete(String tableName){
+        if(!checkParams(tableName)) return null;
+
+        return FUNC_DELETE + PARAM_FROM + tableName;
+    }
+
+    // DROP DATABASE database
+    // DROP TABLE table
     public String drop(){
+        return null;
+    }
+
+    // ALTER table ...
+    public String alter(){
         return null;
     }
 
@@ -75,7 +129,7 @@ public class QueryCreater {
         return "UPDATE SQLITE_SEQUENCE SET seq = "+initIdx+" WHERE name = '"+tableName+"'";
     }
 
-    private String wrap1(ArrayList<Row> rows){
+    private String wrapCreateTable(ArrayList<Row> rows){
         StringBuilder sb = new StringBuilder("(");
 
         if(rows == null || rows.size() <1) return null;
@@ -103,7 +157,7 @@ public class QueryCreater {
         return sb.toString();
     }
 
-    private String wrap2(ArrayList<Row> rows){
+    private String wrapUnit(ArrayList<Row> rows){
         StringBuilder sb = new StringBuilder("(");
 
         if(rows == null || rows.size() <1) return null;
@@ -124,7 +178,7 @@ public class QueryCreater {
         return sb.toString();
     }
 
-    private String wrap3(ArrayList<String> val){
+    private String wrapBase(ArrayList<String> val){
         StringBuilder sb = new StringBuilder("(");
 
         if(val == null || val.size() <1) return null;
@@ -141,8 +195,39 @@ public class QueryCreater {
         return sb.toString();
     }
 
-    public boolean checkEmty(String str){
-        return str != null && str.isEmpty();
+    private String wrapUpdate(ArrayList<UpdateData> datas){
+        StringBuilder sb = new StringBuilder("(");
+
+        if(datas == null || datas.size()<=0) return null;
+
+        int totalCnt = datas.size();
+        for(int idx = 0; idx < totalCnt ; idx++){
+            sb.append(datas.get(idx).toString());
+
+            if(idx != totalCnt-1) sb.append(", ");
+        }
+
+        sb.append(")");
+
+        return sb.toString();
+    }
+
+    private boolean checkEmty(String str){
+        return str == null || str.isEmpty();
+    }
+
+    public boolean checkParams(String... params){
+        for(String param : params){
+            if(checkEmty(param)) return false;
+        }
+        return true;
+    }
+
+    public boolean checkParams(ArrayList... params){
+        for(ArrayList param : params){
+            if(param == null || param.size()<=0) return false;
+        }
+        return true;
     }
 
     public enum Type{
@@ -157,12 +242,22 @@ public class QueryCreater {
         public String getValue(){
             return type;
         }
+        public int getCode(){
+            if(type.equals(INT.getValue())){
+                return 1;
+            }else if(type.equals(TEXT.getValue())){
+                return 10;
+            }else {
+                return -1;
+            }
+        }
     }
 
     public enum Option{
-        PRIMARY_KEY("PRIMARY KEY")
-        , BASE_IDX("PRIMARY KEY AUTOINCREMENT")
-        , NOT_NULL("NOT NULL");
+        PRIMARY_KEY(" PRIMARY KEY ")
+        , BASE_IDX(" PRIMARY KEY AUTOINCREMENT ")
+        , NOT_NULL(" NOT NULL ")
+        , NOT_EXIST(" IF NOT EXIST ");
 
         private final String option;
 
@@ -205,5 +300,69 @@ public class QueryCreater {
             this.option = option.getValue();
         }
 
+    }
+
+    public class UpdateData{
+        private String rowName = null;
+        private Type type = null;
+        private Object data = null;
+
+        public String getRowName() {
+            return rowName;
+        }
+
+        public void setRowName(String rowName) {
+            this.rowName = rowName;
+        }
+
+        public int getiData(int base) {
+            if(check(Type.INT)) return base;
+            return (int)data;
+        }
+
+        public String getStrData() {
+            if(check(Type.TEXT)) return null;
+            return (String)data;
+        }
+
+        public void setData(Object data){
+            this.data = data;
+            setType(data);
+        }
+
+        private void setType(Object data){
+            if(data instanceof Integer){
+                type = Type.INT;
+            }else if(data instanceof String){
+                type = Type.TEXT;
+            }
+        }
+
+        public Type getType(){
+            return type;
+        }
+
+        private boolean check(Type type){
+            return rowName == null || rowName.isEmpty() || type == null || !this.type.equals(type);
+        }
+
+        public String toString(){
+            if(checkEmty(rowName)) return "";
+
+            StringBuilder sb = new StringBuilder();
+            sb.append(rowName).append("=");
+            switch (type.getCode()){
+                case -1:
+                    break;
+                case 1:
+                    sb.append(getiData(-1));
+                    break;
+                case 10:
+                    sb.append("\"").append(getStrData()).append("\"");
+                    break;
+            }
+
+            return sb.toString();
+        }
     }
 }
